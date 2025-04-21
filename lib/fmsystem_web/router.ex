033e0b82793d0b56b6a684d857b2f30512d37f08
Mohammed Almalki <mr.motid@gmail.com>
2025-04-21 18:34:26 +0300

    Feat: Define API routes with authentication and resource endpoints

--- lib/fmsystem_web/router.ex
+++ lib/fmsystem_web/router.ex
@@ -1,11 +1,50 @@
 defmodule FmsystemWeb.Router do
   use FmsystemWeb, :router
+  alias FmsystemWeb.Plugs.VerifyJWT
 
   pipeline :api do
     plug :accepts, ["json"]
   end
 
+  pipeline :api_auth do
+    # Verify JWT and load current_user
+    plug VerifyJWT
+  end
+
   scope "/api", FmsystemWeb do
     pipe_through :api
+    # --- Public Routes ---
+    get "/health", GeneralController, :health
+    # More descriptive action name
+    post "/register", UserController, :register
+    # More descriptive action name
+    post "/login", SessionController, :login
+    post "/telemetry", TelemetryController, :create
+    # --- Protected Routes (require authentication) ---
+    scope "/" do
+      # Add JWT verification pipeline
+      pipe_through :api_auth
+
+      # Session / Current User
+      # Endpoint to get current user info
+      get "/me", SessionController, :show
+
+      # API Auth CRUD
+      get "/api_auth", ApiAuthController, :index
+      post "/api_auth", ApiAuthController, :create
+      # delete "/api_auth/:id", ApiAuthController, :delete # Example delete
+
+      # Vehicle CRUD
+      get "/vehicles", VehicleController, :index
+      post "/vehicles", VehicleController, :create
+      # Example show
+      get "/vehicles/:id", VehicleController, :show
+      # put "/vehicles/:id", VehicleController, :update # Example update
+
+      # IoT CRUD
+      get "/iot", IotController, :index
+      post "/iot", IotController, :create
+      # get "/iot/:id", IotController, :show # Example show
+    end
   end
 end
